36bdf34d48fc40036c36717ed06c68268494e5ce
alexdevteam <alexpro820@gmail.com>
2022-10-01 14:21:21 +0200

    Improve assembler errors

--- assembler/src/lib.rs
+++ assembler/src/lib.rs
@@ -1,18 +1,46 @@
 use std::collections::HashMap;
 
 use nom::branch::alt;
-use nom::bytes::complete::is_a;
+use nom::bytes::complete::{is_a, tag};
 use nom::character::complete::{alpha1, alphanumeric0, alphanumeric1, digit1, newline, space1};
 use nom::character::is_alphabetic;
-use nom::combinator::{map, opt};
+use nom::combinator::{map, map_res, opt};
+use nom::error::{FromExternalError, ParseError};
 use nom::multi::{many1, separated_list0, separated_list1};
 use nom::sequence::{pair, preceded, tuple};
 use nom::IResult;
 use simplez_common::*;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error<I> {
+    UndefinedLabel { name: String },
+    InvalidInstruction { name: String },
+    RedefinedLabel { name: String },
+    MissingParameter,
+    InvalidLabelName,
+    ParseError(#[from] nom::error::Error<I>),
+}
+
+impl<I> ParseError<I> for Error<I> {
+    fn from_error_kind(input: I, kind: nom::error::ErrorKind) -> Self {
+        Self::ParseError(nom::error::Error::from_error_kind(input, kind))
+    }
+
+    fn append(_: I, _: nom::error::ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+
+impl<I> FromExternalError<I, Error<I>> for Error<I> {
+    fn from_external_error(_: I, _: nom::error::ErrorKind, e: Error<I>) -> Self {
+        e
+    }
+}
 
 pub struct AssemblyLine<'s> {
     label: Option<&'s str>,
-    instruction: Instruction<Direction<'s>>,
+    instruction: Option<Instruction<Direction<'s>>>,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -21,73 +49,82 @@ pub enum Direction<'s> {
     Address(Address),
 }
 
-pub fn parse_label<'s>(input: &'s str) -> IResult<&str, &str> {
+pub fn parse_label<'s>(input: &'s str) -> IResult<&str, &str, Error<&str>> {
     if let Some(first_char) = input.bytes().nth(0) {
         if is_alphabetic(first_char) {
             alphanumeric1(input)
         } else {
-            Err(nom::Err::Error(nom::error::Error {
-                input,
-                code: nom::error::ErrorKind::Alpha,
-            }))
+            Err(nom::Err::Error(Error::InvalidLabelName))
         }
     } else {
-        Err(nom::Err::Error(nom::error::Error {
-            input,
-            code: nom::error::ErrorKind::AlphaNumeric,
-        }))
+        Err(nom::Err::Error(Error::InvalidLabelName))
     }
 }
 
-pub fn parse_direction<'s>(input: &'s str) -> IResult<&str, Direction<'s>> {
+pub fn parse_direction<'s>(input: &'s str) -> IResult<&str, Direction<'s>, Error<&str>> {
     alt((
         map(parse_label, |label| Direction::Label(label)),
-        map(digit1, |addr: &str| {
+        map(preceded(opt(tag("/")), digit1), |addr: &str| {
             Direction::Address(Address(addr.parse::<u16>().unwrap()))
         }),
     ))(input)
 }
 
-pub fn parse_assembly_line<'s>(input: &'s str) -> IResult<&str, AssemblyLine<'s>> {
-    map(
+pub fn parse_assembly_line<'s>(input: &'s str) -> IResult<&str, AssemblyLine<'s>, Error<&str>> {
+    map_res(
         tuple((
-            alphanumeric0::<&str, nom::error::Error<&str>>,
+            alphanumeric0,
             space1,
-            alpha1,
-            opt(preceded(space1, separated_list1(space1, parse_direction))),
+            opt(tuple((
+                alpha1,
+                opt(preceded(space1, separated_list1(space1, parse_direction))),
+            ))),
         )),
-        |(label, _, instruction, params): (&str, &str, &str, Option<Vec<Direction<'s>>>)| {
+        |(label, _, instruction): (&str, &str, Option<(&str, Option<Vec<Direction<'s>>>)>)| {
             let label = if label.is_empty() { None } else { Some(label) };
 
-            let instruction = match instruction.to_owned().to_lowercase().as_str() {
-                "st" => Instruction::Store {
-                    address: params.unwrap()[0],
-                },
-                "ld" => Instruction::Load {
-                    address: params.unwrap()[0],
-                },
-                "add" => Instruction::Add {
-                    address: params.unwrap()[0],
-                },
-                "br" => Instruction::Branch {
-                    address: params.unwrap()[0],
-                },
-                "bz" => Instruction::BranchIfZero {
-                    address: params.unwrap()[0],
-                },
-                "clr" => Instruction::Clear,
-                "dec" => Instruction::Decrease,
-                "halt" => Instruction::Halt,
+            let instruction = instruction.map(
+                |(instruction, params)| -> Result<Instruction<Direction>, Error<&'s str>> {
+                    let get_param = || params.map(|p| p[0]).ok_or(Error::MissingParameter);
+                    Ok(match instruction.to_owned().to_lowercase().as_str() {
+                        "st" => Instruction::Store {
+                            address: get_param()?,
+                        },
+                        "ld" => Instruction::Load {
+                            address: get_param()?,
+                        },
+                        "add" => Instruction::Add {
+                            address: get_param()?,
+                        },
+                        "br" => Instruction::Branch {
+                            address: get_param()?,
+                        },
+                        "bz" => Instruction::BranchIfZero {
+                            address: get_param()?,
+                        },
+                        "clr" => Instruction::Clear,
+                        "dec" => Instruction::Decrease,
+                        "halt" => Instruction::Halt,
 
-                _ => panic!(),
+                        other => {
+                            return Err(Error::InvalidInstruction {
+                                name: other.to_string(),
+                            })
+                        }
+                    })
+                },
+            );
+            let instruction = match instruction {
+                Some(x) => Some(x?),
+                None => None,
             };
 
-            AssemblyLine { instruction, label }
+            Ok(AssemblyLine { instruction, label })
         },
     )(input)
 }
 
-pub fn assemble<'s>(input: &'s str) -> IResult<&str, Vec<u16>> {
+pub fn assemble<'s>(input: &'s str) -> IResult<&str, Vec<u16>, Error<&str>> {
     let mut labels = HashMap::new();
     let lines = separated_list0(many1(newline), parse_assembly_line)(input)?.1;
     {
@@ -95,7 +132,9 @@ pub fn assemble<'s>(input: &'s str) -> IResult<&str, Vec<u16>> {
         for line in lines.iter() {
             if let Some(label) = line.label {
                 if labels.contains_key(&label) {
-                    panic!()
+                    return Err(nom::Err::Failure(Error::RedefinedLabel {
+                        name: label.to_string(),
+                    }));
                 }
                 labels.insert(label, current_addr);
             }
@@ -104,33 +143,47 @@ pub fn assemble<'s>(input: &'s str) -> IResult<&str, Vec<u16>> {
         }
     }
 
-    let convert_direction = |dir: Direction| -> Address {
-        dbg!(dir);
+    let convert_direction = |dir: Direction| -> Result<Address, Error<&str>> {
         match dir {
-            Direction::Address(addr) => addr,
-            Direction::Label(label) => labels[label],
+            Direction::Address(addr) => Ok(addr),
+            Direction::Label(label) => {
+                labels
+                    .get(&label)
+                    .copied()
+                    .ok_or_else(|| Error::UndefinedLabel {
+                        name: label.to_string(),
+                    })
+            }
         }
     };
-
-    dbg!(&labels);
 
     Ok((
         input,
         lines
             .into_iter()
-            .map(|line| match line.instruction {
-                Instruction::Store { address } => convert_direction(address).0 & 0o777,
-                Instruction::Load { address } => (1 << 9) | convert_direction(address).0 & 0o777,
-                Instruction::Add { address } => (2 << 9) | convert_direction(address).0 & 0o777,
-                Instruction::Branch { address } => (3 << 9) | convert_direction(address).0 & 0o777,
-                Instruction::BranchIfZero { address } => {
-                    (4 << 9) | convert_direction(address).0 & 0o777
-                }
-                Instruction::Clear => 5 << 9,
-                Instruction::Decrease => 6 << 9,
-                Instruction::Halt => 7 << 9,
+            .filter_map(|line| line.instruction)
+            .map(|instruction| -> Result<_, _> {
+                Ok(match instruction {
+                    Instruction::Store { address } => convert_direction(address)?.0 & 0o777,
+                    Instruction::Load { address } => {
+                        (1 << 9) | convert_direction(address)?.0 & 0o777
+                    }
+                    Instruction::Add { address } => {
+                        (2 << 9) | convert_direction(address)?.0 & 0o777
+                    }
+                    Instruction::Branch { address } => {
+                        (3 << 9) | convert_direction(address)?.0 & 0o777
+                    }
+                    Instruction::BranchIfZero { address } => {
+                        (4 << 9) | convert_direction(address)?.0 & 0o777
+                    }
+                    Instruction::Clear => 5 << 9,
+                    Instruction::Decrease => 6 << 9,
+                    Instruction::Halt => 7 << 9,
+                })
             })
-            .collect(),
+            .collect::<Result<Vec<u16>, Error<&str>>>()
+            .map_err(|err| nom::Err::Failure(err))?,
     ))
 }
 
@@ -140,7 +193,19 @@ fn test() {
     let asm = include_str!("../../test.sz").to_owned();
     let result = assemble(&asm).unwrap().1;
     let lines = asm.lines();
-    for (word, line) in result.iter().zip(lines) {
-        println!("{:04o} {}", word, line);
+    let mut words = result.iter();
+
+    for line in lines {
+        let asm_instruction = parse_assembly_line(line)
+            .ok()
+            .map(|line| line.1.instruction);
+        if asm_instruction.is_some() {
+            println!("{:04o} {}", words.next().unwrap(), line);
+        } else {
+            println!("     {}", line);
+        }
     }
+
+    // All lines with instructions should map 1:1 to words
+    assert!(words.next() == None);
 }
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+
+#[derive(Clone, Copy, Debug)]
 pub enum Instruction<Addr = Address> {
     Store { address: Addr },
     Load { address: Addr },
@@ -9,9 +12,48 @@ pub enum Instruction<Addr = Address> {
     Halt,
 }
 
+impl From<u16> for Instruction<Address> {
+    fn from(val: u16) -> Self {
+        let ins = (val >> 9) & 0o7;
+        let param = Address(val & 0o777);
+        match ins {
+            0 => Instruction::Store { address: param },
+            1 => Instruction::Load { address: param },
+            2 => Instruction::Add { address: param },
+            3 => Instruction::Branch { address: param },
+            4 => Instruction::BranchIfZero { address: param },
+            5 => Instruction::Clear,
+            6 => Instruction::Decrease,
+            7 => Instruction::Halt,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Display for Instruction<Address> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::Store { address } => f.write_fmt(format_args!("ST {}", address)),
+            Instruction::Load { address } => f.write_fmt(format_args!("LD {}", address)),
+            Instruction::Add { address } => f.write_fmt(format_args!("ADD {}", address)),
+            Instruction::Branch { address } => f.write_fmt(format_args!("BR {}", address)),
+            Instruction::BranchIfZero { address } => f.write_fmt(format_args!("BZ {}", address)),
+            Instruction::Clear => f.write_str("CLR"),
+            Instruction::Decrease => f.write_str("DEC"),
+            Instruction::Halt => f.write_str("HALT"),
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct Address(pub u16);
 
 impl Address {
     pub const ZERO: Self = Self(0);
+}
+
+impl Display for Address {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("/{}", self.0))
+    }
 }
